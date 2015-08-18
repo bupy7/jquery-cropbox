6a4da3b5610c8c2e6ae41cbce46b6ad9cbeb47dc
Vasilij Belosludcev <bupy765@gmail.com>
2015-08-18 12:24:32 +0500

    more code

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+java -jar /path/to/yuicompressor.jar ./../src/jquery.cropbox.js -o ./../dist/jquery.cropbox.min.js
+java -jar /path/to/yuicompressor.jar ./../src/jquery.cropbox.css -o ./../dist/jquery.cropbox.min.css
